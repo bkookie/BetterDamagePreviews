4a86888e30d0f6445b452c1d6e255363d3993e33
chunker <github.diving763@passmail.net>
2026-08-24 12:44:06 +1000

    Add project files.

---
+++ BetterDamagePreviewsCode/Placeholder.cs
@@ -0,0 +1,6 @@
+﻿namespace BetterDamagePreviews.BetterDamagePreviewsCode;
+
+public class Placeholder
+{
+    //You're recommended but not required to keep all your code in this package and all your assets in the BetterDamagePreviews folder.
+}
